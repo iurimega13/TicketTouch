f7816e4dad75d2a62ae2e3dbcb43701f1a68fb02
Iuri Galdino <iurimega13@gmail.com>
2024-02-16 18:55:42 -0300

    Criando módulo, controlador e service do Adm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AdminsModule } from './admins/admins.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, AdminsModule],
   controllers: [AppController],
   providers: [AppService],
 })
