5e464003adb319ad5b85cc99a0159f4cce554f92
Iuri Galdino <iurimega13@gmail.com>
2024-02-16 19:01:04 -0300

    Criando módulo e service de notificação

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './users/users.module';
 import { AdminsModule } from './admins/admins.module';
 import { TechniciansModule } from './technicians/technicians.module';
 import { TicketsModule } from './tickets/tickets.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [UsersModule, AdminsModule, TechniciansModule, TicketsModule],
+  imports: [UsersModule, AdminsModule, TechniciansModule, TicketsModule, NotificationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
