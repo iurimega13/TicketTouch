c85fe6fe3e126d70f454c10c9516af7ccf87b669
Iuri Galdino <iurimega13@gmail.com>
2024-02-16 19:22:03 -0300

    Realizando alguns testes de GET no módulo users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    @Get()
+    async getUsers() {
+        return JSON.stringify({ test: 'Get all users' });
+    }
+}
