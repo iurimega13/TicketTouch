50622c9bf5343b08e84975896ff201de783a8a3e
Iuri Galdino <iurimega13@gmail.com>
2024-02-17 15:50:20 -0300

    Criando conexão do BD

--- src/users/user.module.ts
+++ src/users/user.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local'],
+    }),
+    TypeOrmModule.forRoot({
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      password: `mysecretpassword`,
+      port: parseInt(process.env.DB_PORT),
+      username: `postgres`,
+      type: 'postgres',
+    }),
+    UserModule,
+  ],
   controllers: [UserController],
   providers: [UserService]
 })
