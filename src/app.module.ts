c53bce48b3036553cd56f8298cd5f6a843f50e20
Iuri Galdino <iurimega13@gmail.com>
2024-02-18 18:20:01 -0300

    Add comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,36 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './users/user.module';
-import { AdminsModule } from './users/admins.module';
-import { TechniciansModule } from './users/technicians.module';
-import { TicketsModule } from './tickets/tickets.module';
-import { NotificationsModule } from './notifications/notifications.module';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { EquipmentsModule } from './equipaments/equipments.module';
+import { Module } from '@nestjs/common'; // Importa o decorator Module do NestJS
+import { AppController } from './app.controller'; // Importa o controlador AppController
+import { AppService } from './app.service'; // Importa o serviço AppService
+import { UserModule } from './users/user.module'; // Importa o módulo UserModule
+import { AdminsModule } from './users/admins.module'; // Importa o módulo AdminsModule
+import { TechniciansModule } from './users/technicians.module'; // Importa o módulo TechniciansModule
+import { TicketsModule } from './tickets/tickets.module'; // Importa o módulo TicketsModule
+import { NotificationsModule } from './notifications/notifications.module'; // Importa o módulo NotificationsModule
+import { ConfigModule } from '@nestjs/config'; // Importa o módulo ConfigModule do NestJS
+import { TypeOrmModule } from '@nestjs/typeorm'; // Importa o módulo TypeOrmModule do NestJS
+import { EquipmentsModule } from './equipaments/equipments.module'; // Importa o módulo EquipmentsModule
+import { AuthModule } from './auth/auth.module'; // Importa o módulo AuthModule
 
 @Module({
   imports: [
-    
+    // Configuração do ambiente e banco de dados
     ConfigModule.forRoot({
-      envFilePath: ['.env.development.local'],
+      envFilePath: ['.env.development.local'], // Define o arquivo de ambiente a ser carregado
     }),
     TypeOrmModule.forRoot({
-      database: `tickettouch`,
-      host: `localhost`,
-      password: `mysecretpassword`,
-      port: 5432,
-      username: `postgres`,
-      type: 'postgres',
-      synchronize: false,
-      entities: ['dist/**/*.entity{.ts,.js}'],
+      database: `tickettouch`, // Nome do banco de dados
+      host: `localhost`, // Host do banco de dados
+      password: `mysecretpassword`, // Senha do banco de dados
+      port: 5432, // Porta do banco de dados
+      username: `postgres`, // Nome de usuário do banco de dados
+      type: 'postgres', // Tipo do banco de dados
+      synchronize: false, // Sincroniza automaticamente o esquema do banco de dados com as entidades
+      entities: ['dist/**/*.entity{.ts,.js}'], // Localização das entidades
     }),
-    UserModule, AdminsModule, TechniciansModule, TicketsModule, NotificationsModule],
-  controllers: [AppController],
-  providers: [AppService],
+    // Importação dos módulos
+    UserModule, AdminsModule, TechniciansModule, TicketsModule, NotificationsModule, AuthModule
+  ],
+  controllers: [AppController], // Controladores fornecidos pelo módulo
+  providers: [AppService], // Serviços fornecidos pelo módulo
 })
-export class AppModule {}
+export class AppModule {} // Define a classe AppModule como módulo principal da aplicação
