e3c7fef59c539f315c7c0ab6328c4d7fd09a8eb5
Iuri Galdino <iurimega13@gmail.com>
2024-02-18 18:19:38 -0300

    Add comments

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from '@nestjs/common'; // Importa decorators e módulos do NestJS
+import { AppService } from './app.service'; // Importa o serviço AppService
 
-@Controller()
+@Controller() // Define este como o controlador principal
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {} // Injeta o serviço AppService no construtor
 
-  @Get()
+  @Get() // Define um método GET para a rota raiz
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHello(); // Retorna a mensagem de boas-vindas obtida do serviço AppService
   }
 }
