f2e996f9c1787e21d3ef2a35f8993b682b709a89
Iuri Galdino <iurimega13@gmail.com>
2024-02-18 23:23:52 -0300

    Ajustes na estrutura

--- src/users/technicians.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TechniciansController } from './technicians.controller';
-import { TechniciansService } from './technicians.service';
-
-@Module({
-  controllers: [TechniciansController],
-  providers: [TechniciansService]
-})
-export class TechniciansModule {}
