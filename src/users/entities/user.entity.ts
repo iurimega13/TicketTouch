e7e495ecb2f248207eab7793b483d6b971bc2bca
Iuri Galdino <iurimega13@gmail.com>
2024-02-19 22:01:59 -0300

    Correção para seguir padrão do BD

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,18 +1,20 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn, CreateDateColumn, ManyToOne, JoinColumn } from "typeorm";
+import { UnitEntity } from "src/units/entities/unit.entity";
+import { DepartamentEntity } from "src/departaments/entities/departament.entity";
 
 
 @Entity({name: 'users'})
 export class UserEntity {
-    @PrimaryGeneratedColumn('rowid')
+    @PrimaryGeneratedColumn()
     id : number;
 
-    @Column({name: 'registration', nullable: false})
-    registration: string;
+    @Column({name: 'registration', nullable: false, unique: true})
+    registration: number;
 
     @Column({name: 'name', nullable: false})
     name: string;
 
-    @Column({name: 'email', nullable: false})
+    @Column({name: 'email', nullable: false, unique: true})
     email: string;
 
     @Column({name: 'password', nullable: false})
@@ -24,4 +26,14 @@ export class UserEntity {
     @Column({name: 'phone_number'})
     phone_number: string;
 
+    @ManyToOne(() => UnitEntity)
+    @JoinColumn({name: 'unit_id'})
+    unit: UnitEntity;
+
+    @ManyToOne(() => DepartamentEntity)
+    @JoinColumn({name: 'department_id'})
+    department: DepartamentEntity;
+
+    @CreateDateColumn({name: 'created_at', type: 'timestamp'})
+    created_at: Date;
 }
