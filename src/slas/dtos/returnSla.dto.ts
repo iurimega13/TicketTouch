36a95de632fd67b76b09c5dcfff0ee0b8a2a43bc
Iuri Galdino <iurimega13@gmail.com>
2024-02-20 20:55:18 -0300

    Criado dto de Return de SLA

---
+++ src/slas/dtos/returnSla.dto.ts
@@ -0,0 +1,19 @@
+import { SlaEntity } from "../entities/sla.entity";
+
+
+export class ReturnSlaDto {
+
+    id: number;
+    name: string;
+    description: string;
+    response_time: number;
+    resolution_time: number;
+
+    constructor(sla: SlaEntity) {
+        this.id = sla.id;
+        this.name = sla.name;
+        this.description = sla.description;
+        this.response_time = sla.response_time;
+        this.resolution_time = sla.resolution_time;
+    }
+}
