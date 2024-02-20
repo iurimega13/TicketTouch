b4720d8e827a31ba292a7a8ce87c5a463b0e6fcc
Iuri Galdino <iurimega13@gmail.com>
2024-02-19 22:00:47 -0300

    Add entity

---
+++ src/tickets/entities/ticket.entity.ts
@@ -0,0 +1,53 @@
+import { SlaEntity } from "src/slas/entities/sla.entity";
+import { CategoryEntity } from "src/ticket-categories/entities/category.entity";
+import { UnitEntity } from "src/units/entities/unit.entity";
+import { UserEntity } from "src/users/entities/user.entity";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+
+@Entity({name: 'tickets'}) // Define o nome da tabela no banco de dados
+export class TicketEntity {
+    @PrimaryGeneratedColumn() // Define a coluna como chave primária e auto incrementável
+    id: number; // Define o tipo da coluna como número
+
+    @Column({name: 'title', nullable: false})
+    title: string; // Define o tipo da coluna como string
+
+    @Column({name: 'description', nullable: false})
+    description: string; // Define o tipo da coluna como string
+    
+    @Column({name: 'priority', nullable: false})
+    priority: string; // Define o tipo da coluna como string
+
+    @Column({name: 'status', nullable: false})
+    status: string; // Define o tipo da coluna como string
+
+    @ManyToOne(() => CategoryEntity)
+    @JoinColumn({name: 'category_id'})
+    category: CategoryEntity;
+    
+    @ManyToOne(() => UserEntity)
+    @JoinColumn({name: 'user_id'})
+    user: UserEntity;
+    
+    @ManyToOne(() => UserEntity)
+    @JoinColumn({name: 'technician_id'})
+    technician: UserEntity;
+    
+    @ManyToOne(() => UnitEntity)
+    @JoinColumn({name: 'unit_id'})
+    unit: UnitEntity;
+    
+    @ManyToOne(() => SlaEntity)
+    @JoinColumn({name: 'sla_id'})
+    sla: SlaEntity;
+
+    @Column({name: 'due_date', nullable: false})
+    due_date: Date; // Define o tipo da coluna como data
+
+    @CreateDateColumn({name: 'created_at', nullable: false})
+    created_at: Date; // Define o tipo da coluna como data
+
+    @UpdateDateColumn({name: 'updated_at', nullable: false})
+    updated_at: Date; // Define o tipo da coluna como data
+}
