d782d11dfdf8762dbe827db56225ac4f19a141a5
Iuri Galdino <iurimega13@gmail.com>
2024-02-19 22:01:22 -0300

    Add entity

---
+++ src/user-feedback/entities/feedback.entity.ts
@@ -0,0 +1,28 @@
+import { TicketEntity } from "src/tickets/entities/ticket.entity";
+import { UserEntity } from "src/users/entities/user.entity";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('feedback')
+export class FeedbackEntity {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @ManyToOne(() => UserEntity)
+    @JoinColumn({name: 'user_id'})
+    user: UserEntity;
+
+    @ManyToOne(() => TicketEntity)
+    @JoinColumn({name: 'ticket_id'})
+    ticket: TicketEntity;
+
+    @Column({name: 'rating', nullable: false})
+    rating: number;
+
+    @Column({name: 'comment', nullable: false})
+    comment: string;
+
+    @CreateDateColumn({name: 'created_at', nullable: false})
+    created_at: Date;
+
+}
