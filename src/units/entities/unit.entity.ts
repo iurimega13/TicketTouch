42ea315bc934baf4a85552c9926d6e5f5568d8d4
Iuri Galdino <iurimega13@gmail.com>
2024-02-19 22:01:03 -0300

    Add entity

---
+++ src/units/entities/unit.entity.ts
@@ -0,0 +1,20 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity({name: 'units'})
+export class UnitEntity {
+ 
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({name: 'name', nullable: false})
+    name: string;
+
+    @Column({name: 'address', nullable: false})
+    address: string;
+
+    @CreateDateColumn({name: 'created_at', nullable: false})
+    created_at: Date;
+
+    @UpdateDateColumn({name: 'updated_at', nullable: false})
+    updated_at: Date;
+}
