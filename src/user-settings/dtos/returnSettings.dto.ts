2eaa1f6ad308d0fca52ea88021e55e851c18d2d2
Iuri Galdino <iurimega13@gmail.com>
2024-02-20 21:59:14 -0300

    Criado dto para return das settings

---
+++ src/user-settings/dtos/returnSettings.dto.ts
@@ -0,0 +1,21 @@
+import { UserEntity } from "src/users/entities/user.entity";
+import { SettingsEntity } from "../entities/settings.entity";
+
+
+export class ReturnSettingsDto {
+    id: number;
+    user_id: UserEntity;
+    theme: string;
+    notifications_settings: string;
+    created_at: Date;
+    updated_at: Date;
+
+    constructor(settings: SettingsEntity) {
+        this.id = settings.id;
+        this.user_id = settings.user;
+        this.theme = settings.theme;
+        this.notifications_settings = settings.notifications_settings;
+        this.created_at = settings.created_at;
+        this.updated_at = settings.updated_at;
+    }
+}
