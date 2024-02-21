f2a67de5bb7f8dc12152f86578213d067477ac47
Iuri Galdino <iurimega13@gmail.com>
2024-02-20 21:38:27 -0300

    Criado dto para return da unidade

---
+++ src/units/dtos/returnUnit.dto.ts
@@ -0,0 +1,18 @@
+import { UnitEntity } from "../entities/unit.entity";
+
+
+export class ReturnUnitDto {
+    id: number;
+    name: string;
+    address: string;
+    created_at: Date;
+    updated_at: Date;
+
+    constructor(unit: UnitEntity){
+        this.id = unit.id;
+        this.name = unit.name;
+        this.address = unit.address;
+        this.created_at = unit.created_at;
+        this.updated_at = unit.updated_at;
+    }
+}
