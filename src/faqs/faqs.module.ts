bd5c261edc7df6194625f9144d8af826a2fbf7d7
Iuri Galdino <iurimega13@gmail.com>
2024-02-20 23:22:13 -0300

    Atualizando os imports

--- src/faqs/faqs.module.ts
+++ src/faqs/faqs.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { FaqsService } from './faqs.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FaqsController } from './faqs.controller';
+import { FaqEntity } from 'src/faqs/entities/faq.entity';
 
 @Module({
-  providers: [FaqsService]
+  imports: [
+    TypeOrmModule.forFeature([FaqEntity])  
+  ],
+  controllers: [FaqsController],
+  providers: [FaqsService],
+  exports: [FaqsService]
 })
 export class FaqsModule {}
