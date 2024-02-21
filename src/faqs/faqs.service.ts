b7b1196ad515e174f60859ec84dee95063a5f7e8
Iuri Galdino <iurimega13@gmail.com>
2024-02-20 23:23:54 -0300

    Adicionado os metodos create, update , getall e getbyid

--- src/faqs/faqs.service.ts
+++ src/faqs/faqs.service.ts
@@ -1,4 +1,72 @@
+import { UpdateFaqDto } from './dtos/updateFaq.dto';
 import { Injectable } from '@nestjs/common';
+import { FaqEntity } from './entities/faq.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateFaqDto } from './dtos/createFaq.dto';
 
 @Injectable()
-export class FaqsService {}
+export class FaqsService {
+
+    constructor(
+        @InjectRepository(FaqEntity)
+        private readonly faqRepository: Repository<FaqEntity>,
+    ) {}
+
+    async createFaq(createFaqDto: CreateFaqDto): Promise<FaqEntity> {
+        try {
+            const now = new Date();
+
+            const faq = {
+                ...createFaqDto,
+                createdAt: now,
+                updatedAt: now,
+            };
+            return await this.faqRepository.save(faq);
+            }
+        catch (error) {
+            throw new Error(error);
+        }
+    }
+
+
+    async updateFaq( updateFaqDto: UpdateFaqDto): Promise<FaqEntity> {
+        try {
+            const faq = await this.getFaqById(updateFaqDto.id);
+            if (!faq) {
+                throw new Error('FAQ not found');
+            }
+
+            const updatedFaq = {
+                ...faq,
+                ...updateFaqDto,
+                updatedAt: new Date(),
+            };
+
+            return await this.faqRepository.save(updatedFaq);
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
+
+    async getAllFaqs(): Promise<FaqEntity[]> {
+        try {
+            return await this.faqRepository.find();
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
+
+
+    async getFaqById(id: number): Promise<FaqEntity> {
+        try {
+            const faq = await this.faqRepository.findOne({ where: { id }});
+            if (!faq) {
+                throw new Error('FAQ not found');
+            }
+            return faq;
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
+}
