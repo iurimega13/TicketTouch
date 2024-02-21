dbefc4bacd07a01c402d7ba3662698e6182934d4
Iuri Galdino <iurimega13@gmail.com>
2024-02-20 23:23:07 -0300

    Adicionado oas chamadas para os metodos create, update e getall

--- src/faqs/faqs.controller.ts
+++ src/faqs/faqs.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { CreateFaqDto } from './dtos/createFaq.dto';
+import { ReturnFaqDto } from './dtos/returnFaq.dto';
+import { FaqEntity } from './entities/faq.entity';
+import { FaqsService } from './faqs.service';
+import { UpdateFaqDto } from './dtos/updateFaq.dto';
 
 @Controller('faqs')
-export class FaqsController {}
+export class FaqsController {
+
+    constructor(private readonly faqsService: FaqsService) {}
+
+    @UsePipes(ValidationPipe)
+    @Post()
+    async createFaq(@Body() createFaqDto: CreateFaqDto): Promise<FaqEntity> {
+        return this.faqsService.createFaq(createFaqDto);
+    }
+
+    @UsePipes(ValidationPipe)
+    @Post()
+    async updateFaq(@Body() updateFaqDto: UpdateFaqDto): Promise<FaqEntity> {
+        return this.faqsService.updateFaq(updateFaqDto);
+    }
+
+    @Get()
+    async getAllFaqs(): Promise<ReturnFaqDto[]> {
+        return (await this.faqsService.getAllFaqs()).map((FaqEntity) => new ReturnFaqDto(FaqEntity));
+    }
+}
