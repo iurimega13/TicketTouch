855408d001bb77b1e72c51de706db6c858cfa4e5
Iuri Galdino <iurimega13@gmail.com>
2024-02-21 23:20:50 -0300

    Add imports

--- src/units/units.module.ts
+++ src/units/units.module.ts
@@ -6,7 +6,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([UnitEntity])    ],
+        TypeOrmModule.forFeature([UnitEntity])   ],
     controllers: [UnitsController],
     providers: [UnitsService],
     exports: [UnitsService]
