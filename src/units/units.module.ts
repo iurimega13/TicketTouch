a397119016022d7e55fb6946efe9645b84edcabe
Iuri Galdino <iurimega13@gmail.com>
2024-02-21 21:09:40 -0300

    Adicionado importes do module units

--- src/units/units.module.ts
+++ src/units/units.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { UnitEntity } from './entities/unit.entity';
+import { UnitsController } from './units.controller';
+import { UnitsService } from './units.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+    imports: [
+        TypeOrmModule.forFeature([UnitEntity])    ],
+    controllers: [UnitsController],
+    providers: [UnitsService],
+    exports: [UnitsService]
+})
 export class UnitsModule {}
