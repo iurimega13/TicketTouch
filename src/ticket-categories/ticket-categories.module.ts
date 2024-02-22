d6061a8c254d3ed46e4bbe6231a15542550cb46c
Iuri Galdino <iurimega13@gmail.com>
2024-02-22 20:41:34 -0300

    Add imports

--- src/ticket-categories/ticket-categories.module.ts
+++ src/ticket-categories/ticket-categories.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TicketCategoryEntity } from './entities/ticketCategory.entity';
+import { TicketCategoriesController } from './ticket-categories.controller';
+import { TicketCategoriesService } from './ticket-categories.service';
 
-@Module({})
+@Module({
+    imports: [
+        TypeOrmModule.forFeature([TicketCategoryEntity]),
+    ],
+    controllers: [TicketCategoriesController],
+    providers: [TicketCategoriesService],
+    exports: [TicketCategoriesService],
+})
 export class TicketCategoriesModule {}
