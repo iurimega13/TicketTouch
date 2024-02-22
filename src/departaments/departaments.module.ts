95d19b0b7d22d7d39eb8d3b1e28a8a84fd291991
Iuri Galdino <iurimega13@gmail.com>
2024-02-21 22:34:49 -0300

    Add imports

--- src/departaments/departaments.module.ts
+++ src/departaments/departaments.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DepartamentsController } from './departaments.controller';
+import { DepartamentsService } from './departaments.service';
+import { DepartamentEntity } from './entities/departament.entity';
 
-@Module({})
+@Module({
+    imports: [
+        TypeOrmModule.forFeature([DepartamentEntity])
+    ],
+    controllers: [DepartamentsController],
+    providers: [DepartamentsService],
+    exports: [DepartamentsService],
+})
 export class DepartamentsModule {}
