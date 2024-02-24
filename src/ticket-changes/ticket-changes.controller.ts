8a65bf102a853b76e81ae47adfd7b2e70ebd8bc5
Iuri Galdino <iurimega13@gmail.com>
2024-02-23 21:34:47 -0300

    Add chamada dos metodos

--- src/ticket-changes/ticket-changes.controller.ts
+++ src/ticket-changes/ticket-changes.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { CreateChangeDto } from './dtos/createChange.dto';
+import { ReturnChangeDto } from './dtos/returnChange.dto';
+import { TicketChangeEntity } from './entities/ticketChanges.entity';
+import { TicketChangesService } from './ticket-changes.service';
+import { Body, Controller, Get, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 
 @Controller('ticket-changes')
-export class TicketChangesController {}
+export class TicketChangesController {
+
+    constructor(private readonly ticketChangesService: TicketChangesService) {}
+
+    @UsePipes(ValidationPipe)
+    @Post()
+    async createChange(@Body() createChange: CreateChangeDto): Promise<TicketChangeEntity> {
+        return this.ticketChangesService.createChange(createChange);
+    }
+
+    @Get(':ticket_id')
+    async getChangesByTicketId(@Param('ticket_id') ticket_id: number): Promise<ReturnChangeDto[]> {
+        return this.ticketChangesService.getChangesByTicketId(ticket_id);
+    }
+}
