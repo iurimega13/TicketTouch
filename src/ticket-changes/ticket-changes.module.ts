6c0a20c78a911472c57e32a1f51d74dd710c64e2
Iuri Galdino <iurimega13@gmail.com>
2024-02-23 21:34:32 -0300

    Add imports

--- src/ticket-changes/ticket-changes.module.ts
+++ src/ticket-changes/ticket-changes.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TicketChangesController } from './ticket-changes.controller';
+import { TicketChangesService } from './ticket-changes.service';
+import { TicketChangeEntity } from './entities/ticketChanges.entity';
 
-@Module({})
+@Module({
+    imports: [
+        TypeOrmModule.forFeature([TicketChangeEntity]),
+    ],
+    controllers: [TicketChangesController],
+    providers: [TicketChangesService],
+    exports: [TicketChangesService],
+})
 export class TicketChangesModule {}
