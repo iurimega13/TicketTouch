484aa6758b4b2c4db92a2e21ca91cdbb6816e145
Iuri Galdino <iurimega13@gmail.com>
2024-03-04 21:06:04 -0300

    Refatorando forma de chamada de dados da .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,20 +4,16 @@ import { AppService } from './app.service'; // Importa o serviço AppService
 import { UserModule } from './users/user.module'; // Importa o módulo UserModule
 import { TicketsModule } from './tickets/tickets.module'; // Importa o módulo TicketsModule
 import { NotificationsModule } from './notifications/notifications.module'; // Importa o módulo NotificationsModule
-import { ConfigModule } from '@nestjs/config'; // Importa o módulo ConfigModule do NestJS
+import { ConfigModule, ConfigService } from '@nestjs/config'; // Importa o módulo ConfigModule do NestJS
 import { TypeOrmModule } from '@nestjs/typeorm'; // Importa o módulo TypeOrmModule do NestJS
 import { EquipmentsModule } from './equipments/equipments.module'; // Importa o módulo EquipmentsModule
 import { AuthModule } from './auth/auth.module'; // Importa o módulo AuthModule
 import { APP_GUARD } from '@nestjs/core';  // Importa o APP_GUARD do NestJS
 import { RolesGuard } from './guards/roles.guards'; // Importa o guard RolesGuard
-import { UnitsService } from './units/units.service';  // Importa o serviço UnitsService
-import { DepartamentsService } from './departaments/departaments.service'; // Importa o serviço DepartamentsService
 import { TicketCategoriesModule } from './ticket-categories/ticket-categories.module'; // Importa o módulo TicketCategoriesModule
 import { AttachmentsModule } from './attachments/attachments.module'; // Importa o módulo AttachmentsModule
 import { TicketChangesModule } from './ticket-changes/ticket-changes.module'; // Importa o módulo TicketChangesModule
-import { UserFeedbackService } from './user-feedback/user-feedback.service'; // Importa o serviço UserFeedbackService
 import { SlasModule } from './slas/slas.module'; // Importa o módulo SlasModule
-import { UserSettingsService } from './user-settings/user-settings.service'; // Importa o serviço UserSettingsService
 import { FaqsModule } from './faqs/faqs.module'; // Importa o módulo FaqsModule
 import { DepartamentsModule } from './departaments/departaments.module';
 import { UnitsModule } from './units/units.module';
@@ -30,13 +26,15 @@ import { UnitsModule } from './units/units.module';
       envFilePath: ['.env.development.local'], // Define o arquivo de ambiente a ser carregado
     }),
     TypeOrmModule.forRootAsync({
-      useFactory: () => ({
+      imports: [ConfigModule], 
+      inject: [ConfigService], 
+      useFactory: (configService: ConfigService) => ({
         type: 'postgres', // Tipo do banco de dados
-        database: process.env.DB_DATABASE, // Nome do banco de dados
-        host: process.env.DB_HOST, // Host do banco de dados
-        password: process.env.DB_PASSWORD, // Senha do banco de dados
-        port: parseInt(process.env.DB_PORT), // Porta do banco de dados
-        username: process.env.DB_USERNAME, // Nome de usuário do banco de dados
+        database: configService.get('DB_DATABASE'), // Nome do banco de dados
+        host: configService.get('DB_HOST'), // Host do banco de dados
+        password: configService.get('DB_PASSWORD'), // Senha do banco de dados
+        port: Number(configService.get('DB_PORT')), // Porta do banco de dados
+        username: configService.get('DB_USERNAME'), // Nome de usuário do banco de dados
         synchronize: false, // Sincroniza automaticamente o esquema do banco de dados com as entidades
         entities: ['dist/**/*.entity{.ts,.js}'], // Localização das entidades
       }),
