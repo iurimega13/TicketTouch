4a79ba0489de3dd6f10ec180d22b27e6325c252e
Iuri Galdino <iurimega13@gmail.com>
2024-03-04 21:20:38 -0300

    Add codigo para seguir padrao orm

--- src/faqs/entities/faq.entity.ts
+++ src/faqs/entities/faq.entity.ts
@@ -1,13 +1,23 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { IsNotEmpty, Length } from "class-validator";
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
 
 @Entity({ name: 'faqs' })
 export class FaqEntity {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
   @Column()
+  @IsNotEmpty()
+  @Length(10, 1000)
   question: string;
 
   @Column()
+  @Length(10, 1000)
   answer: string;
+  
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
 }
