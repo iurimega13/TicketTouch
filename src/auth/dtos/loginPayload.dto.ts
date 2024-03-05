073d92f2df210143d0cdac2e68b79f781e291c63
iuri galdino <36307698+iurimega13@users.noreply.github.com>
2024-03-05 17:24:04 +0000

    Corrigindo tipagem do user.id

--- src/auth/dtos/loginPayload.dto.ts
+++ src/auth/dtos/loginPayload.dto.ts
@@ -5,7 +5,7 @@ export class LoginPayload {
     role: string; // Define a propriedade role do payload de login
 
     constructor (user: UserEntity) { // Construtor da classe LoginPayload que recebe um objeto do tipo UserEntity como parâmetro
-        this.registration = user.registration; // Atribui o registro do usuário ao registro do payload de login
+        this.registration = String(user.registration); // Atribui o registro do usuário ao registro do payload de login, convertendo-o para string
         this.role = user.role; // Atribui a função do usuário ao papel do payload de login
     }
 }
