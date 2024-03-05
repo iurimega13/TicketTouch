5bcaf0e08c169fba578bdd922a4314836ddc3c35
Iuri Galdino <iurimega13@gmail.com>
2024-03-04 21:18:49 -0300

    Add feats necessárias

--- src/user-feedback/user-feedback.module.ts
+++ src/user-feedback/user-feedback.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UserFeedbackController } from './user-feedback.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FeedbackEntity } from './entities/feedback.entity';
+import { UserFeedbackService } from './user-feedback.service';
 
 @Module({
-  controllers: [UserFeedbackController]
+  imports: [
+    TypeOrmModule.forFeature([FeedbackEntity]),
+  ],
+  controllers: [UserFeedbackController],
+  providers: [UserFeedbackService],
+  exports: [UserFeedbackService],
 })
 export class UserFeedbackModule {}
