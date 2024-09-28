8c9afb80fc5eff95ed8ba1ff4fb573a74ce3d404
Iuri De Araujo <iurimega13@gmail.com>
2024-09-28 20:04:52 -0300

    Add cors para conexao com front

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: '*' });
+
+  // Definir o prefixo global para todas as rotas
+  app.setGlobalPrefix('api');
+
+  // Configuração do CORS
+  app.enableCors({
+    origin: 'http://localhost:3000', // Substitua com a URL do seu frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos HTTP permitidos
+    credentials: true, // Permitir envio de cookies ou outros headers de autenticação
+  });
+
+  // Iniciar a aplicação na porta 3001
   await app.listen(3001);
 }
 bootstrap();
