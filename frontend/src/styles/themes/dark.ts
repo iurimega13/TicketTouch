c475dd9c1f92bd1ef0a24ab19d8ff64483e93f0c
Iuri De Araujo <iurimega13@gmail.com>
2024-09-28 20:06:41 -0300

    Ajuste nos temas

--- frontend/src/styles/themes/dark.ts
+++ frontend/src/styles/themes/dark.ts
@@ -2,7 +2,7 @@ export default {
     title: "dark",
         colors: {
         primary: "#1F1F1F",
-        secundary: "#CCCCCC",
+        secundary: "#495057",
         tertiary: "#CCCCCC",
         background: "#121212",
         text: "#FFF",
--- frontend/src/styles/themes/light.ts
+++ frontend/src/styles/themes/light.ts
@@ -4,7 +4,8 @@ export default {
         primary: "#012A4A",
         secundary: "#006DAA",
         tertiary: "#CCCCCC",
-        background: "#F0F0F0",
-        text: "#CCCCCC",
+        background: "#e5f6ff",
+        text: "#F0F0F0",
+        textHover: "#FFF",
     },  
 };
