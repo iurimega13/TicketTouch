5ae3c3a2a2de8fbd277414e12271bd8da0de9fe9
Iuri De Araujo <iurimega13@gmail.com>
2024-10-02 00:23:38 -0300

    Ajuste na logica de login e dto

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,37 +1,35 @@
-import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common'; // Importa o decorator Injectable e NotFoundException do NestJS
-import { LoginDto } from './dtos/login.dto'; // Importa o DTO para login
-import { UserService } from '../users/user.service'; // Importa o serviço UserService
-import { compare } from 'bcrypt'; // Importa a função compare do bcrypt para comparar senhas
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { LoginDto } from './dtos/login.dto';
+import { UserService } from '../users/user.service';
+import { compare } from 'bcrypt';
 import { ReturnLogin } from './dtos/returnLogin.dto';
-import { JwtService } from '@nestjs/jwt'; // Importa a classe JwtService do pacote @nestjs/jwt
+import { JwtService } from '@nestjs/jwt';
 import { ReturnUserDto } from '../users/dtos/returnUser.dto';
 import { LoginPayload } from './dtos/loginPayload.dto';
 
-@Injectable() 
+@Injectable()
 export class AuthService {
-    constructor(private readonly userService: UserService, private readonly jwtService: JwtService) {} // Injeta o serviço UserService e JwtService no construtor
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
 
-    async login(LoginDto: LoginDto): Promise<ReturnLogin> {
-        // Busca o usuário pelo registro fornecido no DTO de login
-        const user = await this.userService.findByUsername(LoginDto.username);
-        
-        // Se o usuário não for encontrado, lança NotFoundException
-        if (!user) {
-            throw new NotFoundException(`Usuário com registro ${LoginDto.username} não encontrado`);
-        }
-    
-        // Compara a senha fornecida com a senha armazenada no banco de dados
-        const isMatch = await compare(LoginDto.password, user.password);
-    
-        // Se a senha estiver incorreta, lança UnauthorizedException
-        if (!isMatch) {
-            throw new UnauthorizedException('Senha incorreta'); 
-        }
-        
-        // Retorna um objeto com o token JWT e o usuário
-        return {
-            acessToken: this.jwtService.sign( {...new LoginPayload(user)} ), // Gera um token JWT com base no payload do usuário
-            user: new ReturnUserDto(user) // Retorna o usuário
-        };
+  async login(loginDto: LoginDto): Promise<ReturnLogin> {
+    const user = await this.userService.findByUsername(loginDto.username);
+
+    if (!user) {
+      throw new NotFoundException(`Usuário com registro ${loginDto.username} não encontrado`);
     }
+
+    const isMatch = await compare(loginDto.password, user.password);
+
+    if (!isMatch) {
+      throw new UnauthorizedException('Senha incorreta');
+    }
+
+    return {
+      accessToken: this.jwtService.sign({ ...new LoginPayload(user) }),
+      user: new ReturnUserDto(user),
+    };
+  }
 }
--- backend/src/auth/dtos/returnLogin.dto.ts
+++ backend/src/auth/dtos/returnLogin.dto.ts
@@ -1,6 +1,6 @@
 import { ReturnUserDto } from "src/users/dtos/returnUser.dto"; // Importa a interface ReturnUserDto do diretório de DTOs de retorno de usuário
 
-export interface ReturnLogin { // Define a interface ReturnLogin
-    user: ReturnUserDto; // Define uma propriedade user que é do tipo ReturnUserDto
-    acessToken: string; // Define uma propriedade accessToken que é do tipo string
-}
+export interface ReturnLogin {
+    accessToken: string;
+    user: ReturnUserDto;
+  }
