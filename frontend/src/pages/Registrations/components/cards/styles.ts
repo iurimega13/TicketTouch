09dc3074f2a675841dd8c3ca5e299330ac524610
iuri galdino <36307698+iurimega13@users.noreply.github.com>
2024-10-24 19:52:49 +0000

    Refatorar cor de fundo do componente de cartões de registro

--- frontend/src/pages/Registrations/components/cards/styles.ts
+++ frontend/src/pages/Registrations/components/cards/styles.ts
@@ -7,7 +7,7 @@ export const Card = styled.div`
   border-radius: 4px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
   padding: 16px;
-  background-color: ${props => props.theme.colors.secundary};
+  background-color: ${props => props.theme.colors.primary} !important;
   margin: 16px 0;
 `;
 
