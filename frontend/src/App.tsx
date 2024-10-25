99bd4b4a65e2bc448e38273205e4d05a31cebf36
iuri galdino <36307698+iurimega13@users.noreply.github.com>
2024-10-25 00:07:09 +0000

    Refatorar importações e rotas no componente App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, useLocation, Routes, Route, Navigate } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  useLocation,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import Cookies from 'js-cookie';
 
@@ -14,6 +20,9 @@ import Registrations from './pages/Registrations';
 import NotFound from './pages/NotFound';
 import PrivateRoute from './components/PrivateRoute';
 import { getUserSettings, updateUserSettings } from './services/api';
+import Faqs from './pages/Faqs';
+import Support from './pages/Support';
+import Tickets from './pages/Tickets';
 
 const Layout: React.FC = () => {
   const location = useLocation();
@@ -51,7 +60,9 @@ const Layout: React.FC = () => {
     const userId = localStorage.getItem('userId');
     if (userId) {
       try {
-        await updateUserSettings({ theme: newTheme === dark ? 'dark' : 'light' });
+        await updateUserSettings({
+          theme: newTheme === dark ? 'dark' : 'light',
+        });
       } catch (error) {
         console.error('Erro ao atualizar o tema do usuário:', error);
       }
@@ -68,27 +79,95 @@ const Layout: React.FC = () => {
     <ThemeProvider theme={theme}>
       <div className="App">
         <GlobalStyle />
-        <Navbar isvisible={showNavbar} toggleTheme={toggleTheme} isDarkMode={theme === dark} setTheme={setTheme} />
+        <Navbar
+          isvisible={showNavbar}
+          toggleTheme={toggleTheme}
+          isDarkMode={theme === dark}
+          setTheme={setTheme}
+        />
         <Routes>
-          <Route path="/" element={<Navigate to={isAuthenticated ? "/home" : "/login"} />} />
-          <Route path="/login" element={<Login onLoginSuccess={handleLoginSuccess} setTheme={setTheme} />} />
+          <Route
+            path="/"
+            element={<Navigate to={isAuthenticated ? '/home' : '/login'} />}
+          />
+          <Route
+            path="/login"
+            element={
+              <Login onLoginSuccess={handleLoginSuccess} setTheme={setTheme} />
+            }
+          />
+
           {isAuthenticated !== null && (
             <>
               <Route
                 path="/home"
-                element={<PrivateRoute element={<Home />} isAuthenticated={isAuthenticated!} />}
+                element={
+                  <PrivateRoute
+                    element={<Home />}
+                    isAuthenticated={isAuthenticated!}
+                    allowedRoles={['admin', 'analyst', 'user']}
+                  />
+                }
               />
               <Route
                 path="/profile-settings"
-                element={<PrivateRoute element={<ProfileSettings />} isAuthenticated={isAuthenticated!} />}
+                element={
+                  <PrivateRoute
+                    element={<ProfileSettings />}
+                    isAuthenticated={isAuthenticated!}
+                    allowedRoles={['admin', 'analyst', 'user']}
+                  />
+                }
               />
               <Route
                 path="/registrations"
-                element={<PrivateRoute element={<Registrations />} isAuthenticated={isAuthenticated!} />}
+                element={
+                  <PrivateRoute
+                    element={<Registrations />}
+                    isAuthenticated={isAuthenticated!}
+                    allowedRoles={['admin']}
+                  />
+                }
+              />
+              <Route
+                path="/tickets"
+                element={
+                  <PrivateRoute
+                    element={<Tickets />}
+                    isAuthenticated={isAuthenticated!}
+                    allowedRoles={['admin', 'analyst', 'user']}
+                  />
+                }
+              />
+              <Route
+                path="/support"
+                element={
+                  <PrivateRoute
+                    element={<Support />}
+                    isAuthenticated={isAuthenticated!}
+                    allowedRoles={['admin', 'analyst']}
+                  />
+                }
+              />
+              <Route
+                path="/faqs"
+                element={
+                  <PrivateRoute
+                    element={<Faqs />}
+                    isAuthenticated={isAuthenticated!}
+                    allowedRoles={['admin', 'analyst', 'user']}
+                  />
+                }
               />
               <Route
                 path="*"
-                element={isAuthenticated ? <NotFound /> : <Navigate to="/login" replace={true} />}
+                element={
+                  isAuthenticated ? (
+                    <NotFound />
+                  ) : (
+                    <Navigate to="/login" replace={true} />
+                  )
+                }
               />
             </>
           )}
