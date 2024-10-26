e6588068121808a10779962bd450bf2d00a56ceb
iuri galdino <36307698+iurimega13@users.noreply.github.com>
2024-10-26 00:57:10 +0000

    Refactorizar DTO de atualização de FAQ para permitir campos opcionais

--- backend/src/faqs/dtos/updateFaq.dto.ts
+++ backend/src/faqs/dtos/updateFaq.dto.ts
@@ -1,10 +1,12 @@
-import { IsNumber, IsString } from "class-validator";
+import { IsOptional, IsString } from "class-validator";
 
 
 export class UpdateFaqDto {
     @IsString()
+    @IsOptional()
     question?: string;
 
+    @IsOptional()
     @IsString()
     answer?: string;
   }
