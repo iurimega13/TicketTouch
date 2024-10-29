5afd23a941d64b9e9d2dae2ed5fd713b02401bf4
iuri galdino <36307698+iurimega13@users.noreply.github.com>
2024-10-29 00:30:56 +0000

    Refactorizar tipos de dados de tickets

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -5,3 +5,15 @@ export interface User {
     setor: string;
     unidade: string;
   }
+
+export interface TicketData {
+  title: string;
+  description: string;
+  priority: string;
+  status: string;
+  category_id?: string;
+  sla_id?: string;
+  user_id?: string;
+  analyst_id?: string;
+  unit_id?: string;
+}
