b815b58acd5dc23d7bb9915b84614834e76556c7
iuri galdino <36307698+iurimega13@users.noreply.github.com>
2024-11-12 22:23:15 +0000

    Adicionar novos campos ao TicketData e interface Change; melhorar a estrutura de dados para gerenciamento de tickets

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -1,19 +1,35 @@
 export interface User {
-    id: string;
-    username: string;
-    role: string;
-    setor: string;
-    unidade: string;
-  }
+  id: string;
+  username: string;
+  role: string;
+  setor: string;
+  unidade: string;
+}
 
 export interface TicketData {
+  key: string;
+  id: string;
   title: string;
   description: string;
   priority: string;
   status: string;
-  category_id?: string;
-  sla_id?: string;
-  user_id?: string;
-  analyst_id?: string;
-  unit_id?: string;
+  created_at: string;
+  updated_at: string;
+  sla_id: string;
+  user: string;
+  unit: string;
+  category_name?: string;
+  technician?: User;
+  department?: string;
+  type?: string;
+  assignee?: string;
+  changes?: Change[];
+}
+
+
+export interface Change {
+  field: string;
+  value: string;
+  date?: string;
+  username?: string;
 }
